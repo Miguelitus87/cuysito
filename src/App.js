07874e9dcba347771ce58a9399fa7082dcb89f86
Miguel Angel Ramirez Echarry <90867689+Miguelitus87@users.noreply.github.com>
2022-02-08 21:12:22 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer'
+import ItemCount from './components/ItemCount'
 
 function App() {
   return (
     <>
     <NavBar />
-    <ItemListContainer saludo="Hola aca va a ir la lista de los productos"/>
-    
+    <ItemListContainer saludo="Hola aca va a ir la lista de los productos" />
+    <ItemCount/>
     </>
   );
 }
