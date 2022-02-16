128f6647d3a17d679807f584605c540ab5838704
Miguel Angel Ramirez Echarry <90867689+Miguelitus87@users.noreply.github.com>
2022-02-15 23:58:02 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 
 import './App.css';
-import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer'
-import { useState } from 'react';
+import { NavBar } from './components/NavBar';
+import { ItemListContainer } from './components/ItemListContainer'
+import { ItemDetailContainer } from './components/ItemDetailContainer';
+
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   
-
   
-
   return (
     <>
-    <NavBar />
-    <ItemListContainer/>
     
+      <BrowserRouter>
+        <NavBar/>
+
+        <Routes>
+          <Route path='/' element={<ItemListContainer/>} />
+        
+          <Route path='/detail' element={<ItemDetailContainer/>} />
+        </Routes>
     
+      </BrowserRouter>
     </>
   );
 }
