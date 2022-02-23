4453ca29c06fb70b9661991063071bb2893b5412
Miguel Angel Ramirez Echarry <90867689+Miguelitus87@users.noreply.github.com>
2022-02-23 00:10:32 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import { ItemListContainer } from './components/ItemListContainer'
 import { ItemDetailContainer } from './components/ItemDetailContainer';
 
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { Cart } from './components/Cart';
 
 function App() {    
+  
+  
   return (
     <>
       <BrowserRouter>
+
         <NavBar/>
+
         <Routes>
           <Route path='/' element={<ItemListContainer/>} />
           <Route path='/categoria/:idCategoria' element={<ItemListContainer/>} />
+          
           <Route path='/item/:id' element={<ItemDetailContainer/>} />    
+          <Route path='/cart/' element={<Cart/>} />   
         </Routes>
       </BrowserRouter>
     </>
@@ -22,3 +29,4 @@ function App() {
 }
 
 export default App; 
+
