e872baa16b330edbc9527ed3a17d09d23534b612
Miguel Angel Ramirez Echarry <90867689+Miguelitus87@users.noreply.github.com>
2022-03-11 03:33:02 -0300

    Add files via upload

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,47 +1,62 @@
 import {useState, createContext} from "react";
+import  db  from "../utils/firebase";
 
 export const CartContext = createContext();
 
 const CartContextProvider = ({children})=>{
     
-    const [CartList, setCartList] = useState([]);
+    const [cartList, setCartList] = useState([]);
+    
+    const isInCart = (idProduct) => {
+        return cartList.find(
+            (item) => item.id === idProduct
+        ) ? true : false;
+    }
 
     const addItem = (item, quantity) => {
+
         console.log('Se ejecuta AddItem en cartContext');
-        console.log(CartList);
-        const isInCart = CartList.find((CartListItem) => CartListItem.id === item.id);
-        if(isInCart) {
-            console.log('Condicion isInCart');
-            setCartList(
-                CartList.map((itemId) => {
-                    return { ...itemId, quantity: itemId.quantity + 1 };
-                })
-            );
-            
+        
+        console.log(item);
+        console.log(quantity);
+
+        if (isInCart(item)) {
+            console.log('Condicion If');
+            for (const products of cartList) {
+                if (products.id === item) {
+                        products.stock += quantity;
+                }
+            }
+            setCartList([...cartList]);
         } else {
             console.log('Condicion else');
-            setCartList([...CartList, item]);            
+            setCartList([
+                ...cartList,   //spread: agrega al contenido existente
+                {
+                    id: item.id,
+                    title: item.title,
+                    description: item.description,
+                    price: item.price,
+                    pictureURL: item.pictureURL 
+                }
+            ]);        
         }        
-        
     };
 
-    const removeItem = (id) => {
-        setCartList(CartList.filter((itemId) => itemId.id !== id ));
-    };
+    const removeItem = (idProduct) => {
+        setCartList(cartList.filter(item => (item.id !== idProduct)));
+    }
 
     const checkout = () => {
         console.log('se ejecuta checkout');
         setCartList([]);
     }
 
-    const prueba = () => {
-        alert('se ejecuta prueba');
-        console.log('se ejecuta prueba');
-        
-    }
+
+
 
     return (    
-        <CartContext.Provider value={{CartList, addItem, removeItem, checkout, prueba }}>
+        <CartContext.Provider value={{cartList, addItem, removeItem, checkout }}>
             {children}
         </CartContext.Provider>
     );
